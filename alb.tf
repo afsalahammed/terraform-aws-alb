1f2e1af2673aa0a381f667d25b399027d8f46326
Afsal Ahammed <afsal.ahammed011@gmail.com>
2025-02-26 20:18:09 +0530

    Added ALB and SG for it.

---
+++ alb.tf
@@ -0,0 +1,13 @@
+resource "aws_lb" "app_alb" {
+  name               = "app-load-balancer"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = var.public_subnets
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "AppALB"
+  }
+}
---
+++ security.tf
@@ -0,0 +1,36 @@
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-security-group"
+  description = "Allow HTTP and HTTPS access"
+  vpc_id      = var.vpc_id
+
+  tags = {
+    Name = "ALB-SG"
+  }
+}
+
+resource "aws_security_group_rule" "alb_ingress_http" {
+  security_group_id = aws_security_group.alb_sg.id
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP from anywhere
+}
+
+resource "aws_security_group_rule" "alb_ingress_https" {
+  security_group_id = aws_security_group.alb_sg.id
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]  # Allow HTTPS from anywhere
+}
+
+resource "aws_security_group_rule" "alb_egress" {
+  security_group_id = aws_security_group.alb_sg.id
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]  # Allow outbound traffic
+}
