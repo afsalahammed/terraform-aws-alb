64393e61539ad1a83941abfc00eca2d5396e3e2b
Afsal Ahammed <afsal.ahammed011@gmail.com>
2025-02-28 15:36:43 +0530

    Added networking and modified output.

---
+++ network.tf
@@ -0,0 +1,26 @@
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.app_vpc.id
+
+  tags = {
+    Name = "AppInternetGateway"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.app_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "PublicRouteTable"
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  for_each       = aws_subnet.public_subnets
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public_rt.id
+}
--- output.tf
+++ output.tf
@@ -32,3 +32,8 @@ output "public_subnet_ids" {
   description = "List of public subnet IDs"
   value       = [for subnet in aws_subnet.public_subnets : subnet.id]
 }
+
+output "internet_gateway_id" {
+  description = "The ID of the Internet Gateway"
+  value       = aws_internet_gateway.igw.id
+}
